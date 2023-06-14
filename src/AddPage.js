2de4090675c59bf9464dc8abfe1d93388994b144
Michael Bogosta <bogostamicheal@gmail.com>
2023-06-14 13:13:07 +0200

    Fix submit response

--- src/AddPage.js
+++ src/AddPage.js
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Alert, Button } from '@mui/material'
 import { Typography, Input } from '@mui/material'
 import { Container, Stack } from '@mui/system'
 import { addDoc, collection } from 'firebase/firestore';
@@ -7,15 +7,17 @@ import React, { useState } from 'react'
 
 export default function AddPage(props) {
     const [value, setValue] = useState("");
+    const [visible, setVisible] = useState(false);
   return (
     <Container>
         <Stack gap={2}>
+            {visible ? <Alert severity='success'>Frage abgeschickt</Alert>:<></>}
             <Typography textAlign={"center"} variant="h1">Gratulation!</Typography>
             <Typography textAlign={"center"} variant="h4">Du hast die Umfrage geschafft</Typography>
             <Typography textAlign={"center"} variant="h5">Jetzt darfst du deine eigene Frage erstellen</Typography>
             <Typography textAlign={"center"} variant="h5">Bitte keine persönlichen Fragen</Typography>
             <Input onChange={(e)=>{setValue(e.target.value)}} id="question_input" placeholder="Deine Frage..."></Input>
-            <Button onClick={()=>{addDoc(collection(props.db, "questions"), {answers:[], question:value, times_answered:0})}}>Senden!</Button>
+            <Button onClick={!visible ? ()=>{setVisible(true);addDoc(collection(props.db, "questions"), {answers:[], question:value, times_answered:0})} : ()=>{}}>Senden!</Button>
         </Stack>
     </Container>
   )
