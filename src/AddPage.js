576c421154636e9e9454f9dd0b2ddeab88550b56
Michael Bogosta <bogostamicheal@gmail.com>
2023-06-14 12:45:19 +0200

    Make website more responsive

--- src/AddPage.js
+++ src/AddPage.js
@@ -13,6 +13,7 @@ export default function AddPage(props) {
             <Typography textAlign={"center"} variant="h1">Gratulation!</Typography>
             <Typography textAlign={"center"} variant="h4">Du hast die Umfrage geschafft</Typography>
             <Typography textAlign={"center"} variant="h5">Jetzt darfst du deine eigene Frage erstellen</Typography>
+            <Typography textAlign={"center"} variant="h5">Bitte keine persönlichen Fragen</Typography>
             <Input onChange={(e)=>{setValue(e.target.value)}} id="question_input" placeholder="Deine Frage..."></Input>
             <Button onClick={()=>{addDoc(collection(props.db, "questions"), {answers:[], question:value, times_answered:0})}}>Senden!</Button>
         </Stack>
